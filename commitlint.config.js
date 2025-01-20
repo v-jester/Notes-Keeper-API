e1306c6594b21af6347e29b6364060e6194906ff
Sashko Vysokovskiy <thettboy11@gmail.com>
2025-01-20 17:12:30 +0200

    ci: fix pull and commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'ci', 'build'],
+    ],
+    'type-case': [2, 'always', 'lower'],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'always'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
 };
